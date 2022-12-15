9351ffb1dd21795ee7d7e5ff53b2f412a2341ab8
Haris Salman Al-Ghifary <haris.solo68@gmail.com>
2022-12-15 20:51:07 +0700

    [INIT]: initialize springboot project
    using spring web, jpa, and spring security

---
+++ src/main/java/com/backendtest/dansmultipro/DansMultiproApplication.java
@@ -0,0 +1,13 @@
+package com.backendtest.dansmultipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DansMultiproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DansMultiproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendtest/dansmultipro/DansMultiproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendtest.dansmultipro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DansMultiproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
